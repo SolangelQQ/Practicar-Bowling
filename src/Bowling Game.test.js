db271dfb7d067ae2031a2ee9df2bbe9bde3e7f8b
Jhoselin Quiroga <jhoselin.quiroga@ucb.edu.bo>
2023-09-22 20:33:00 -0400

    Second test

--- src/Bowling Game.test.js
+++ src/Bowling Game.test.js
@@ -5,10 +5,18 @@ describe("Bowling Game Tests", () => {
   beforeEach (() => {
     g = new Game();
   });
-  it("deberia devolver 0 porque el jugador no derribo ningun pin en los 10 intentos", () => {
+  it("deberia devolver 0 porque el jugador no derribo ningun pin en los 10 turnos", () => {
     for (let i =0; i<20; i++){
       g.roll(0);
     }
     expect(g.score()).toEqual(0);
   });
+
+  it("deberia devolver 20 porque el jugador no derribo un pin en los 20 intentos", () => {
+    for (let i =0; i<20; i++){
+      g.roll(1);
+    }
+    expect(g.score()).toEqual(20);
+  });
+
 });
